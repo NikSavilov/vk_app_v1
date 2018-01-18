d6d0175950615576d12d964033485d93861443c2
NikSavilov <31826550+NikSavilov@users.noreply.github.com>
2018-01-18 23:58:33 +0400

    Added plot creator

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import requests, random, time, traceback, itertools, math
 import datetime
-
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+plotly.tools.set_credentials_file(username='niksavilov', api_key='Qxc4TAOOwk8CU1os7wNx')
+global access_token
+access_token = 'ffb882a0affcbaadecfc56f7cbeeac328fded8f4c01f59d2981c46643fa9e1151ed2f865c11fb1fe64566'
 def get(url, params={}, timeout=5, max_retries=5, backoff_factor=1.3):
     """ Выполнить GET-запрос
 
@@ -38,7 +43,7 @@ def get_friends(user_id, fields):
     assert isinstance(fields, str), "fields must be string"
     assert user_id > 0, "user_id must be positive integer"
     domain = "https://api.vk.com/method"
-    access_token = '0e9fa23ea73d58d59ddd387ad104c25e5c0a716f96d7af7373cd2152492ecb9c4c092d3252a31527cce1d'
+    access_token = 'ffb882a0affcbaadecfc56f7cbeeac328fded8f4c01f59d2981c46643fa9e1151ed2f865c11fb1fe64566'
     # user_id = 141839173
 
     query_params = {
@@ -55,7 +60,6 @@ def get_friends(user_id, fields):
     except:
         print("Invalid user id or access_token")
         exit(1)
-
 def age_predict(user_id):
     assert isinstance(user_id, int), "user_id must be positive integer"
     assert user_id > 0, "user_id must be positive integer"
@@ -70,4 +74,74 @@ def age_predict(user_id):
             count += 1
     return round(int(summa.days)/(365*count))
 
-print('Possible age is', age_predict(141839173))
+def messages_get_history(user_id, offset=0, count=200):
+    assert isinstance(user_id, int), "user_id must be positive integer"
+    assert user_id > 0, "user_id must be positive integer"
+    assert isinstance(offset, int), "offset must be positive integer"
+    assert offset >= 0, "offset must be positive integer"
+    assert count >= 0, "count must be positive integer"
+    domain = "https://api.vk.com/method"
+    # user_id = 141839173
+
+    query_params = {
+    'domain' : domain,
+    'access_token': access_token,
+    'user_id': user_id,
+    'count' : count,
+    'offset' : offset
+    }
+
+    query = "{domain}/messages.getHistory?access_token={access_token}&user_id={user_id}&offset={offset}&count={count}&v=5.53".format(**query_params)
+    response = get(query)
+    try:
+        return response.json()['response']
+    except:
+        print("Invalid user id or access_token")
+        exit(1)
+
+def count_dates_from_messages(messages):
+    dates_arr = []
+    for k in messages:
+        date = datetime.datetime.fromtimestamp(k['date']).strftime("%Y.%m.%d")
+        dates_arr.append(date)
+    dates_list = list(set(dates_arr))
+    frequency = []
+    for j in dates_list:
+        frequency.append({'date' : j, 'messages' : dates_arr.count(j)})
+    frequency_sorted = sorted(frequency, key = lambda x: x['date'])
+    return frequency_sorted
+
+def messages_aggregator(user_id):
+    offset = 0
+    messages = []
+    while True:
+        response = messages_get_history(user_id = user_id, offset = offset)
+        print(response.get('count'))
+        if response['items']:
+            for g in response['items']:
+                messages.append(g)
+        else:
+            return messages
+        offset += 200
+        time.sleep(0.4)
+
+def plot_maker(dates_list):
+    x = []
+    y = []
+    for j in dates_list:
+        x.append(datetime.datetime.strptime(j.get('date'),"%Y.%m.%d"))
+        y.append(j.get('messages'))
+    data = [go.Scatter(x = x, y = y)]
+    py.plot(data)
+    pass
+#print(messages_get_history(user_id = 175747053, offset = 13550))
+#print(messages_get_history(user_id = 175747053, offset = 200))
+
+plot_maker(count_dates_from_messages(messages_aggregator(162674267)))
+# print('Possible age is', age_predict(141839173))
+#print(count_dates_from_messages(messages_get_history(user_id = 162674267, offset = 0)),'\n')
+#time.sleep(1)
+#print(count_dates_from_messages(messages_get_history(user_id = 162674267, offset = 200)),'\n')
+#time.sleep(1)
+#print(count_dates_from_messages(messages_get_history(user_id = 162674267, offset = 400)),'\n')
+# print(dates_aggregator(175747053))
